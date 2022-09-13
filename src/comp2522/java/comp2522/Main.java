cc5f55da131fa94d2d02456c1f11fe0ed8d5d7c3
Purple-Towel <angadgrewal31@gmail.com>
2022-09-12 23:09:09 -0700

    first commit, hello from Major Tom

---
+++ src/comp2522/java/comp2522/Main.java
@@ -0,0 +1,7 @@
+package comp2522;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Major Tom to Ground Control\n" + "I'm stepping through the door\n" + "And I'm floating in a most peculiar way\n" + "And the stars look very different today");
+    }
+}
