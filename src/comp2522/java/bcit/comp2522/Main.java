54efc328640d3c7e990d674468430c8ade94310d
Purple-Towel <angadgrewal31@gmail.com>
2022-09-12 23:25:46 -0700

    second commit, hello again from Major Tom

---
+++ src/comp2522/java/bcit/comp2522/Main.java
@@ -0,0 +1,19 @@
+package bcit.comp2522;
+
+/**
+ * Hello World on-boarding lab for COMP2522.
+ */
+public class Main {
+  /**
+  * Drives the program.
+  *
+  * @param args unused
+  */
+  public static void main(String[] args) {
+    System.out.println(
+                "This is Major Tom to Ground Control\n"
+                + "I'm stepping through the door\n"
+                + "And I'm floating in a most peculiar way\n"
+                + "And the stars look very different today");
+  }
+}
